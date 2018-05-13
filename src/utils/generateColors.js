98175747a782489cfdc467fbfa9fdf349459dfd8
Michael Deeb <git@michaeljdeeb.com>
2018-05-12 19:03:06 -0700

    Accessible colors 🎉

--- src/utils/generateColors.js
+++ src/utils/generateColors.js
@@ -1,5 +1,5 @@
 import randomColor from 'randomcolor';
-// import A11yColor from 'a11ycolor';
+import Color from 'color';
 
 const rgbToHex = (rgbArray) => {
   let hex = '#';
@@ -14,14 +14,38 @@ const rgbToHex = (rgbArray) => {
   return hex;
 };
 
+const nextAccessible = (accent, background, ratio) => {
+  const currentRatio = Color(accent).contrast(Color(background));
+
+  if (currentRatio >= ratio) {
+    return Color(accent).rgb().array();
+  }
+
+  let ratioLighter = currentRatio;
+  let i = 0;
+  while (ratioLighter < ratio && i < 100) {
+    let colorLighter = Color(accent).hsl();
+
+    i += 0.1;
+    colorLighter.color[2] += i;
+
+    colorLighter = Color(colorLighter).hex();
+    ratioLighter = Color(colorLighter).contrast(Color(background));
+
+    if (ratioLighter >= ratio) {
+      return Color(colorLighter).rgb().array();
+    }
+  }
+  return Color(accent).rgb().array();
+};
+
 export default () => {
   const backgroundArray = randomColor({ luminosity: 'dark', format: 'rgbArray' });
   const background = `rgb(${backgroundArray[0]}, ${backgroundArray[1]}, ${backgroundArray[2]})`;
   const backgroundAlpha = `rgba(${backgroundArray[0]}, ${backgroundArray[1]}, ${backgroundArray[2]}, 0.15)`;
 
-  // const dangerousAccent = randomColor({ hue: background, luminosity: 'bright' });
-  // const accent = A11yColor(dangerousAccent, background, 'large', 0.1);
-  const accentArray = randomColor({ hue: rgbToHex(backgroundArray), luminosity: 'bright', format: 'rgbArray' });
+  const dangerousAccent = randomColor({ hue: rgbToHex(backgroundArray), luminosity: 'bright' });
+  const accentArray = nextAccessible(dangerousAccent, background, 3);
   const accent = `rgb(${accentArray[0]}, ${accentArray[1]}, ${accentArray[2]})`;
   const accentAlpha = `rgba(${accentArray[0]}, ${accentArray[1]}, ${accentArray[2]}, 0.15)`;
 
